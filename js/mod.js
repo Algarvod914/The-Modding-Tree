69649aa4285c2384a9b8f6e64350c73328eaf2e3
Algarvod914 <3864345479@qq.com>
2026-03-16 03:46:13 +0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,78 +1,96 @@
 let modInfo = {
-	name: "The ??? Tree",
-	author: "nobody",
-	pointsName: "points",
+	name: "Interstellar Exploration Report: USS Odyssey",
+	id: "uss_odyssey_v1399",
+	author: "Algarvod914",
+	pointsName: "Potential Energy",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
-	offlineLimit: 1,  // In hours
+	initialStartPoints: new Decimal(10), // Start with a bit of juice for the sensors
+	offlineLimit: 1,  // Hours to keep offline production
 }
 
-// Set your version in num and name
-let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
-}
+// Set your game variables
+let doDebug = false
 
-let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
-
-let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
-
-// If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
-// (The ones here are examples, all official functions are already taken care of)
-var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
-
-function getStartPoints(){
-    return new Decimal(modInfo.initialStartPoints)
-}
-
-// Determines if it should show points/sec
-function canGenPoints(){
-	return true
-}
-
-// Calculate points/sec!
+// Use this to determine the point generation
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
+
+	// --- ROW 0 EFFECTS ---
+	if (hasUpgrade("nav", 11)) gain = gain.times(upgradeEffect("nav", 11))
+	if (hasUpgrade("nav", 12)) gain = gain.times(upgradeEffect("nav", 12))
+
+	// --- ROW 1 EFFECTS ---
+	// Note: We use .pow() for stars because they are "Titans of Fusion"
+	if (hasUpgrade("starA", 11)) gain = gain.pow(upgradeEffect("starA", 11))
+	if (getBuyableAmount("starB", 11).gte(1)) gain = gain.pow(buyableEffect("starB", 11))
+
+	// --- ROW 2 EFFECTS (The Encounter) ---
+	if (hasUpgrade("worm", 11)) gain = gain.pow(upgradeEffect("worm", 11))
+	if (hasUpgrade("worm", 12)) gain = gain.times(upgradeEffect("worm", 12)) // Massive static boost
+	if (getBuyableAmount("blackout", 11).gte(1)) gain = gain.pow(buyableEffect("blackout", 11))
+	if (hasUpgrade("science", 11)) gain = gain.pow(upgradeEffect("science", 11))
+
+	// --- ROW 3 EFFECTS (The Logs) ---
+	if (hasUpgrade("log", 11)) gain = gain.pow(upgradeEffect("log", 11))
+	if (hasUpgrade("log", 12)) gain = gain.pow(upgradeEffect("log", 12))
+	if (getBuyableAmount("eval", 11).gte(1)) gain = gain.pow(buyableEffect("eval", 11))
+
+	// --- ROW 4 EFFECTS (The End Game) ---
+	if (getBuyableAmount("end", 11).gte(1)) gain = gain.pow(buyableEffect("end", 11))
+
 	return gain
 }
 
-// You can add non-layer related variables that should to into "player" and be saved here, along with default values
+// Win condition: 10^15,000,000,000
+function winCondition() {
+	return player.points.gte(new Decimal("1e15000000000"))
+}
+
+// Custom display in the "Things" section
+var displayThings = [
+	() => {
+		if (player.points.gt("1e1000000")) return "Status: Ship under tow by USS Hera"
+		if (player.points.gt("1e1000")) return "Status: SOP Updates in progress"
+		if (player.points.gt("1e100")) return "Status: Recovering from Total Blackout"
+		if (player.points.gt("1e10")) return "Status: Entity 'Double Star Worm' attached to hull"
+		return "Status: Spectroscopic survey of V1399 Ophiuchi in progress"
+	},
+	"Current Sector: Sector 7-G, Ophiuchus Constellation"
+]
+
+// Determine when the game "ends"
+function isEndgame() {
+	return player.points.gte(new Decimal("1e15000000000"))
+}
+
+// --- DO NOT EDIT BELOW ---
+
+function canGenPoints(){
+	return true
+}
+
+function gainMult() {
+	return new Decimal(1)
+}
+
+function gainExp() {
+	return new Decimal(1)
+}
+
+function update(diff) {
+}
+
 function addedPlayerData() { return {
 }}
 
-// Display extra things at the top of the page
-var displayThings = [
-]
-
-// Determines when the game "ends"
-function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
-}
-
-
-
-// Less important things beyond this point!
-
-// Style for the background, can be a function
-var backgroundStyle = {
-
-}
-
-// You can change this if you have things that can be messed up by long tick lengths
-function maxTickLength() {
-	return(3600) // Default is 1 hour which is just arbitrarily large
-}
-
-// Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
-// you can cap their current resources with this.
-function fixOldSave(oldVersion){
+function backgroundStyle() {
+	return {
+		"background-color": "#00050a" // Deep space blue/black
+	}
 }
